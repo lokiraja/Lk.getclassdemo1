1c9ae6fc6d1571e546fc7b98ee03d35120a97199
lokiraja <lokesh2kumar9@outlook.com>
2024-09-30 20:25:03 +0530

    Initial commit for web app

---
+++ Getclass1/Models/udata.cs
@@ -0,0 +1,10 @@
+﻿namespace Getclass1.Models
+{
+    public class udata
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string age { get; set; }
+    }
+}
